6734a4c1d4cada6db886b824c1bd281741ec9a0f
L1ndseyHerman <lherman21@jcu.edu>
2021-10-15 01:53:10 -0400

    Caught up to vid478: more useContext().

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoritesCts = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCts.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCts.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
