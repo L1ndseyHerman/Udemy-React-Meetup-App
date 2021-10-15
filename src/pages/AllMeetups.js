47239171b32ea77c97f440af1be5e6cb6f5c6bec
L1ndseyHerman <lherman21@jcu.edu>
2021-10-15 00:40:05 -0400

    Caught up to vid474: useEffect().

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
@@ -6,17 +6,30 @@ function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://udemy-react-review-project-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => {
-      //    Something abt returning the response runs on another thread?
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://udemy-react-review-project-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        //    Something abt returning the response runs on another thread?
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     <section>
