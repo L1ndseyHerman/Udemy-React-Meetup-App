e64b3af5838c6d5139df271f2d412242d5cefd4f
L1ndseyHerman <lherman21@jcu.edu>
2021-10-15 01:17:14 -0400

    Caught up to vid476: functions in the context.

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -8,6 +8,27 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      //  Need to make sure it's based on the prev useState():
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    //    .some() is Vanilla JS!
+    //    It returns true or false depending on whether at least 1 item in the array
+    //    matches the condition or not!
+    //  Returns true if at least one meetup has this meetupId.
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
